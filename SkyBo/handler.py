af152b511d69445e22101917b89e0957c693b6d6
Daniel Maugeri <dcc.maugeri@gmail.com>
2013-05-20 02:01:01 -0400

    Created basics of handler module that will handle skybo's commands
    - also added __init__.py file

---
+++ SkyBo/handler.py
@@ -0,0 +1,15 @@
+'''
+Created on May 19, 2013
+
+@author: daniel
+'''
+
+class CommandHandler:
+    '''
+    classdocs
+    '''
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.comnands = {}
