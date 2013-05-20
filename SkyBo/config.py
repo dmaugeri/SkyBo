75aa50443fe537589f86bf289a5b0a5593e73ded
Daniel Maugeri <dcc.maugeri@gmail.com>
2013-05-20 16:55:54 -0400

    added values for variables for the config

--- SkyBo/config.py
+++ SkyBo/config.py
@@ -4,5 +4,28 @@ Created on May 19, 2013
 @author: daniel
 '''
 
+"""
+TIMEOUT the amount of allotted time in seconds given to
+a script to run before the bot kills the script
+"""
+TIMEOUT = 60
 
-TIMEOUT = 60     
+"""
+CUSTOM_UNIX_SCRIPTS is the path to custom unix scripts
+that you want the bot to execute
+"""
+CUSTOM_UNIX_SCRIPTS = ["~/bin", "/usr/bin"]
+
+
+"""
+COMMANDS is variable that allows you to customize your commands that the bot has
+and which script should be run. If COMMANDS is not defined it just takes all scripts
+under CUSTOM_UNIX_SCRIPTS and the command to run them is the name of the script
+in which case just set COMMANDS = {}
+"""
+COMMANDS = {"command": "/home/daniel/bin/helloworld.sh"}
+
+"""
+LOGFILE is the variable that allows where the logfile should be saved
+"""
+LOGFILE = "logs/skypbo.log"
