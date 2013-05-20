18b11064689b288f45240f787b553e3d0fd0d366
Daniel Maugeri <dcc.maugeri@gmail.com>
2013-05-20 19:10:56 -0400

    added some testing path to the config

--- SkyBo/config.py
+++ SkyBo/config.py
@@ -14,16 +14,18 @@ TIMEOUT = 60
 CUSTOM_UNIX_SCRIPTS is the path to custom unix scripts
 that you want the bot to execute
 """
-CUSTOM_UNIX_SCRIPTS = ["~/bin", "/usr/bin"]
+CUSTOM_UNIX_SCRIPTS = ["/home/daniel/bin/test" ]
 
 
 """
-COMMANDS is variable that allows you to customize your commands that the bot has
+COMMANDS is a variable that allows you to customize your commands that the bot has
 and which script should be run. If COMMANDS is not defined it just takes all scripts
-under CUSTOM_UNIX_SCRIPTS and the command to run them is the name of the script
-in which case just set COMMANDS = {}
+under CUSTOM_UNIX_SCRIPTS and the command to run them is the name of the script (excluding extension)
+
+COMMANDS also overwrite the default behaviour of scripts to execute them by name if both the script is
+included in the directory in CUSTOM_UNIX_SCRIPTS and included in custom COMMANDS
 """
-COMMANDS = {"command": "/home/daniel/bin/helloworld.sh"}
+COMMANDS = {"command": "/home/daniel/bin/test/helloworld.sh"}
 
 """
 LOGFILE is the variable that allows where the logfile should be saved
